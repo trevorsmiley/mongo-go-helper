02fd7e7986357b322f7dccd5781dddee88e8c135
Trevor Smiley <trevor.smiley@gmail.com>
2019-05-31 15:20:28 -0400

    Adding find functions

--- helper.go
+++ helper.go
@@ -2,6 +2,9 @@ package mongohelper
 
 import "go.mongodb.org/mongo-driver/bson"
 import "go.mongodb.org/mongo-driver/bson/primitive"
+import "go.mongodb.org/mongo-driver/mongo"
+import "go.mongodb.org/mongo-driver/mongo/options"
+import "context"
 
 //TextSearchFilter returns a text search filter
 func TextSearchFilter(search string) bson.M {
@@ -23,4 +26,21 @@ func ObjectIdFilter(id string) (bson.M, error) {
 		return nil, err
 	}
 	return bson.M{"_id": oid}, err
+}
+
+//FindOnByObjectId finds one by _id
+func FindOneByObjectId(coll *mongo.Collection, ctx context.Context, objectId string,
+	opts ...*options.FindOneOptions) (*mongo.SingleResult, error) {
+	filter, err := ObjectIdFilter(objectId)
+	if err != nil {
+		return nil, err
+	}
+	r := coll.FindOne(ctx, filter, opts...)
+	return r, err
+}
+
+//FindByTextSearch finds many by a text search
+func FindByTextSearch(coll *mongo.Collection, ctx context.Context, search string,
+	opts ...*options.FindOptions) (*mongo.Cursor, error) {
+	return coll.Find(ctx, TextSearchFilter(search), opts...)
 }
